379e948cd9305cb0cbe8c5818f28d9e8d0f55572
Nickan <colete_nico@yahoo.com>
2014-02-09 22:19:53 +0800

    Removed some unnecessary codes

--- src/framework/Timer.cpp
+++ src/framework/Timer.cpp
@@ -6,15 +6,11 @@ Timer::Timer() {
     previousTime = time.asSeconds();
 }
 
-void Timer::update() {
+float Timer::getDeltaTime() {
     time = clock.getElapsedTime();
     currentTime = time.asSeconds();
     deltaTime = currentTime - previousTime;
     previousTime = currentTime;
-}
-
-float Timer::getDeltaTime() {
-    void update();
 
     ///< Ensures that delta time will always have a positive value
     if (deltaTime < 0) {
