158ace08075332c27e7c64d29cf5fd6c742c1e46
Nickan <colete_nico@yahoo.com>
2014-03-04 16:19:33 +0800

    Might be a temporary holder of Tile infor for tile mapping

---
+++ include/gamescreen/TileInfo.h
@@ -0,0 +1,16 @@
+#ifndef TILEINFO_H
+#define TILEINFO_H
+
+
+class TileInfo
+{
+    public:
+        TileInfo();
+
+        static float tileMapInfo[100];
+        virtual ~TileInfo();
+    protected:
+    private:
+};
+
+#endif // TILEINFO_H
