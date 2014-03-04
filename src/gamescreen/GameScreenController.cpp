eb115532fb9ff151d94b011bfb75078b569d68d1
Nickan <colete_nico@yahoo.com>
2014-03-04 16:18:33 +0800

    GameScreen input controller

---
+++ src/gamescreen/GameScreenController.cpp
@@ -0,0 +1,37 @@
+#include "GameScreenController.h"
+
+GameScreenController::GameScreenController()
+{
+    //ctor
+}
+
+void GameScreenController::keyPressed(const int& keycode) {
+
+}
+
+void GameScreenController::keyReleased(const int& keycode) {
+
+}
+
+void GameScreenController::leftMousePressed(const int& x, const int& y) {
+}
+
+void GameScreenController::leftMouseReleased(const int& x, const int& y) {
+}
+
+void GameScreenController::rightMousePressed(const int& x, const int& y) {
+}
+
+void GameScreenController::rightMouseReleased(const int& x, const int& y) {
+}
+
+
+void GameScreenController::mouseMoved(const int& x, const int& y) {
+}
+
+void GameScreenController::mouseMotion(const int& x, const int& y) {
+}
+
+GameScreenController::~GameScreenController() {
+    //dtor
+}
