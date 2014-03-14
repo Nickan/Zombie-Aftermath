95b4f8589245377f9cec364a1a389d46a1e6a4aa
Nickan <colete_nico@yahoo.com>
2014-03-14 14:27:01 +0800

    For now, only tracks the path given by the path finder and smoothly rotate to the direction it tracks

--- include/model/Zombie.h
+++ include/model/Zombie.h
@@ -2,13 +2,53 @@
 #define ZOMBIE_H
 
 #include "Entity.h"
+#include "MessageDispatcher.h"
+#include "Node.h"
+
+#include <SFML/System/Vector2.hpp>
+#include <cmath>
+#include <vector>
+
+using namespace sf;
+
+//... For detection
+#include <iostream>
+using namespace std;
+
+
+//------------------------------------------------------------------
+//
+// Zombie that knows how to track the node path that is given to it
+//------------------------------------------------------------------
 
 class Zombie : public Entity {
     public:
-        Zombie(FloatRect* boundPtr, float rotation);
+        Zombie(FloatRect* boundPtr, const float& rotation, const float& speed);
+
+        void update(const float& delta);
+
+        void setPath(const vector<Node*>& pathPtrs);
+        const bool handleMessage(Message* msgPtr);
+
+        const float& getLife();
+        const float& getFullLife();
+
+        float stateTime;
         virtual ~Zombie();
     protected:
     private:
+
+        // Will be called when there the zombie is not moving and there is still a path in the path list
+        void setMovement();
+
+        unsigned int worldUnit;
+        float speed;
+        float rotationSpeed;
+        float life;
+        float fullLife;
+        Vector2i velocity;
+        Vector2f previousPos;
+        vector<Node* > pathPtrs;
 };
 
 #endif // ZOMBIE_H
--- src/model/Zombie.cpp
+++ src/model/Zombie.cpp
@@ -1,11 +1,103 @@
 #include "Zombie.h"
 
-Zombie::Zombie(FloatRect* boundPtr, float rotation)
-: Entity(boundPtr, rotation) {
-    //ctor
+Zombie::Zombie(FloatRect* boundPtr, const float& rotation, const float& speed)
+: Entity(boundPtr, rotation), speed(speed) {
+    worldUnit = 32;
+    rotationSpeed = 100.0f;
+    life = 100;
+    fullLife = 100;
 }
 
-Zombie::~Zombie()
-{
+void Zombie::update(const float& delta) {
+    stateTime += delta;
+
+    boundPtr->left += velocity.x * speed * delta;
+    boundPtr->top += velocity.y * speed * delta;
+
+    // If already moved one unit through x
+    if (abs(previousPos.x - boundPtr->left) >= worldUnit) {
+        velocity.x = 0;
+
+        boundPtr->left = round(boundPtr->left / worldUnit) * worldUnit;
+    }
+
+    // If already moved one unit through y
+    if (abs(previousPos.y - boundPtr->top) >= worldUnit) {
+        velocity.y = 0;
+        boundPtr->top = round(boundPtr->top / worldUnit) * worldUnit;
+    }
+
+    // If not moving
+    if ( (velocity.x == 0) && (velocity.y == 0)) {
+
+        // Check if there is path given
+        if (!pathPtrs.empty()) {
+            setMovement();
+        }
+    } else {
+        // Moving, set the rotation
+        float targetRotation = RotationManager::getDegree(velocity.x, velocity.y);
+        rotation = RotationManager::getSmoothRotation(rotation, targetRotation, delta * rotationSpeed);
+    }
+
+}
+
+void Zombie::setMovement() {
+    previousPos.x = boundPtr->left;
+    previousPos.y = boundPtr->top;
+
+    Node* nextNodePtr = pathPtrs.back();
+    pathPtrs.pop_back();
+
+    // Determine the vector to go to the next node
+    velocity.x = nextNodePtr->x - (int) (boundPtr->left / worldUnit);
+    velocity.y = nextNodePtr->y - (int) (boundPtr->top / worldUnit);
+}
+
+const bool Zombie::handleMessage(Message* msgPtr) {
+    switch (msgPtr->msgType) {
+    case MessageType::TARGET_ZOMBIE:
+        MessageDispatcher::sendMessage(id, msgPtr->senderId, 0, MessageType::TARGET_RESPONSE, boundPtr);
+        return true;
+    case MessageType::HIT_ZOMBIE:
+        // Check if still alive
+        if (life >= 0) {
+            // Container for the attack damage
+            Vector2i* atkDmgPtr = (Vector2i*) msgPtr->extraInfo;
+            life -= atkDmgPtr->x;
+
+            // If killed by the attack
+            if (life <= 0) {
+
+            }
+        }
+
+        // Still don't know why I am getting "jump to case label [-fpermissive]" and "crosses initialization"
+        // whenever I remove the parentheses
+        {
+            Vector2i* lifePtr = new Vector2i(life, 0);
+            MessageDispatcher::sendMessage(id, msgPtr->senderId, 0, MessageType::HIT_RESPONSE, lifePtr);
+        }
+        return true;
+    default:
+        return false;
+    }
+}
+
+//------------------ Setters ---------------------
+void Zombie::setPath(const vector<Node*>& pathPtrs) {
+    this->pathPtrs = pathPtrs;
+}
+
+//------------------ Getters ---------------------
+const float& Zombie::getLife() {
+    return life;
+}
+
+const float& Zombie::getFullLife() {
+    return fullLife;
+}
+
+Zombie::~Zombie() {
     //dtor
 }
