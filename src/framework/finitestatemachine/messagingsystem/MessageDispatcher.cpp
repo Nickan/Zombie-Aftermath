4617390ea70db5798d8fad8c71ace694bafac5cd
Nickan <colete_nico@yahoo.com>
2014-03-14 20:06:30 +0800

    Updated

--- src/framework/finitestatemachine/messagingsystem/MessageDispatcher.cpp
+++ src/framework/finitestatemachine/messagingsystem/MessageDispatcher.cpp
@@ -23,24 +23,24 @@ void MessageDispatcher::dischargeMessage(Message* msgPtr, const int& index) {
     // Error checking...
     if (msgPtr->receiverId == -1) {
         cout << "Receiver ID is -1" << endl;
-        delMsgPtr();
+        delMsgPtr(msgPtr, index);
         return;
     } else if (msgPtr->senderId == -1) {
         cout << "Sender ID is -1" << endl;
-        delMsgPtr();
+        delMsgPtr(msgPtr, index);
         return;
     }
 
     BaseEntity* entity = EntityManager::getEntity(msgPtr->receiverId);
 
     if (entity->handleMessage(msgPtr)) {
-        delMsgPtr();
+        delMsgPtr(msgPtr, index);
     }
 }
 
-void MessageDispatcher::delMsgPtr(Message* msgPtr) {
+void MessageDispatcher::delMsgPtr(Message* msgPtr, const int& index) {
     msgPtrList.erase(msgPtrList.begin() + index);
-        delete msgPtr;
+    delete msgPtr;
 }
 
 void MessageDispatcher::sendMessage(const int& senderId, const int& receiverId,
