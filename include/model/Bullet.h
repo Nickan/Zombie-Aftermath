1f6d9e494450be51f4534f48ba3d9f79e119ee4c
Nickan <colete_nico@yahoo.com>
2014-03-14 14:25:09 +0800

    Knows how to track the given target coordinates and detects if it hits it

--- include/model/Bullet.h
+++ include/model/Bullet.h
@@ -1,14 +1,36 @@
 #ifndef BULLET_H
 #define BULLET_H
 
-#include "Entity.h"
+#include <SFML/Graphics/Rect.hpp>
+#include "Vec2.h"
+#include "RotationManager.h"
 
-class Bullet : public Entity {
+//...
+#include <iostream>
+using namespace std;
+
+using namespace sf;
+
+class Bullet {
     public:
         Bullet(FloatRect* boundPtr, float rotation);
         virtual ~Bullet();
+
+        // Returns whether it hits the target
+        const bool targetHit(const float& targetX, const float& targetY);
+
+
+        void trackTarget(const float& targetX, const float& targetY, const float& delta);
+
+        FloatRect* boundPtr;
+        float rotation;
+        float speed;
+
+        bool updating;
+        float boundAllowance;
     protected:
     private:
+        static Vec2 vec2;
 };
 
 #endif // BULLET_H
--- src/model/Bullet.cpp
+++ src/model/Bullet.cpp
@@ -1,11 +1,33 @@
 #include "Bullet.h"
 
+Vec2 Bullet::vec2;
+
 Bullet::Bullet(FloatRect* boundPtr, float rotation)
-: Entity(boundPtr, rotation) {
-    //ctor
+:boundPtr(boundPtr), rotation(rotation) {
+    updating = false;
+    speed = 100.0f;
+    boundAllowance = 5.0f;
 }
 
-Bullet::~Bullet()
-{
+const bool Bullet::targetHit(const float& targetX, const float& targetY) {
+    vec2.x = targetX - boundPtr->left - (boundPtr->width / 2);
+    vec2.y = targetY - boundPtr->top - (boundPtr->height / 2);
+
+    return (vec2.getLengthSqr() < boundAllowance * boundAllowance) ? true : false;
+}
+
+void Bullet::trackTarget(const float& targetX, const float& targetY, const float& delta) {
+    vec2.x = targetX - boundPtr->left - (boundPtr->width / 2);
+    vec2.y = targetY - boundPtr->top - (boundPtr->height / 2);
+    vec2.normalize();
+    rotation = RotationManager::getDegree(vec2.x, vec2.y);
+
+    boundPtr->left += vec2.x * speed * delta;
+    boundPtr->top += vec2.y * speed * delta;
+}
+
+
+
+Bullet::~Bullet() {
     //dtor
 }
