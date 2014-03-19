22b9f829528afb00eff2bb2a5f4b9614877470af
Nickan <colete_nico@yahoo.com>
2014-03-19 22:38:53 +0800

    Updated to latest working basic game. Although there are so many things to add, I don't have time to do them :(

--- src/gamescreen/GameScreen.cpp
+++ src/gamescreen/GameScreen.cpp
@@ -2,14 +2,20 @@
 
 GameScreen::GameScreen(ZombieAftermath* gamePtr) {
     this->gamePtr = gamePtr;
-    setInput(new GameScreenController(this));
 
     updatePtr = new GameScreenUpdate();
     rendererPtr = new GameScreenRenderer(updatePtr);
     updatePtr->initializeMapInfo(rendererPtr->getTileMapInfo());
+
+    setInput(new GameScreenController(this));
 }
 
 void GameScreen::render(RenderWindow& win, const float& delta) {
+    // Don't update if the delta is way too big
+    if (delta > 0.1f) {
+        return;
+    }
+
     updatePtr->update(delta);
     rendererPtr->render(win, delta);
 }
